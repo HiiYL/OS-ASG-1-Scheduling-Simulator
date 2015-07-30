71a6adea94d1917aa532fbc9a5b43affa6e03df3
Yong Lian Hii <Hii@Yongs-MacBook-Pro.local>
2015-07-31 01:31:55 +0800

    Fixed hanging idling

--- roundrobin.sh
+++ roundrobin.sh
@@ -15,15 +15,6 @@ fileDat=($(for each in ${fileDat[@]}; do
   echo $each
 done | sort -n -k4,4nr -k2,2 ))
 unset IFS
-# fileDat=$(for el in "${fileDat[@]}"     ## Sorts input by priority then by arrival time
-# do
-#     echo "$el"
-# done | sort -n -k4,4nr -k2,2 )
-
-
-
-# echo TESTING: ${fileDat[@]}
-# echo ${fileDat[0]}
 
 function printProcess() {
   process=($1)
@@ -57,15 +48,43 @@ currentTime=0
   printf "0 "
 while [ ${#fileDat[@]} -gt 0 ]; do
   count=0
+  # has_element_not_queued=false
+  for process in "$fileDat[@]}"; do
+    processSeparated=($process)
+    if [ -z "${processSeparated[5]}" ]; then
+      has_element_not_queued=true
+      break
+    fi
+  done
+  if ! $has_element_not_queued ; then
+    for process in "$fileDat[@]}"; do
+      processSeparated=($process)
+      unset ${processSeparated[5]}
+      # echo "Unsetting!" $has_element_not_queued
+    done
+  fi
+  # for process in "${fileDat[@]}"; do
+  #   echo $process
+  # done
+
+  unset hasElementUpdated
   for process in "${fileDat[@]}"
     do
       processSeparated=($process)
       arrivalTime=${processSeparated[1]}
       # echo ${processSeparated[0]} and count is $count
       # echo ${processSeparated[0]} - Current Time: $currentTime - ArrivalTime: $arrivalTime
-      if ! [ $arrivalTime -gt $currentTime ] ; then
-
+      # echo Count is: $count
+      if  [ ! $arrivalTime -gt $currentTime ] ; then
+        processSeparated[5]=true
+        hasElementUpdated=false
+        if ! [ -z $idleTime ]; then
+          # echo "TESTING TESTING"
+          echo -n  "[IDLE]" $currentTime' '
+          unset idleTime
+        fi
         let currentTime=currentTime+$(($QUANTUM>${processSeparated[2]} ?${processSeparated[2]}:$QUANTUM))
+
         echo -n  [${processSeparated[0]}] $currentTime' '
         if [ -z "${processSeparated[4]}" ]; then
           let processSeparated[4]=${processSeparated[2]}-$QUANTUM 
@@ -82,9 +101,14 @@ while [ ${#fileDat[@]} -gt 0 ]; do
           fileDat[count]=$process
         fi
       fi
-      sleep 0.250
+      # sleep 0.250
       let count=count+1
   done
+  if [ -z $hasElementUpdated ] ; then
+    idleTime=true
+    let currentTime=currentTime+1
+    unset hasElementUpdated
+  fi
   fileDat=("${fileDat[@]}")
 done
   let averageTurnaroundTime=totalTurnAroundTime/PROCESSCOUNT
