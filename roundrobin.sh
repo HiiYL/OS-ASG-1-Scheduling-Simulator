485d764916e124ae36002ffcc73b2aa7a92caf81
YLHii <yonglian146@gmail.com>
2015-08-25 17:06:25 +0800

    Updated roundrobin.sh

--- roundrobin.sh
+++ roundrobin.sh
@@ -2,87 +2,56 @@
 
 printFile
 
+ARRIVED_PROCESSES=()
 IFS=$'\n' 
 fileDat=($(for each in ${fileDat[@]}; do
   echo $each
-done | sort -n -k4,4nr -k2,2 ))
+done | sort -n -k2,2 ))
 unset IFS
-
 currentTime=0
-  echo
-  echo
-  echo "~~~ Round Robin (RRBN) Scheduling ~~~"
-  echo 
-  echo
-  echo Grantt Chart
-  printf " 0 "
-while [ ${#fileDat[@]} -gt 0 ]; do
-  count=0
-  # has_element_not_queued=false
-  for process in "$fileDat[@]}"; do
+while [ ${#fileDat[@]} -gt 0 ] || [ ${#ARRIVED_PROCESSES[@]} -gt 0 ]; do
+  for index in "${!fileDat[@]}"; do
+    process=${fileDat[$index]}
     processSeparated=($process)
-    if [ -z "${processSeparated[5]}" ]; then
-      has_element_not_queued=true
-      break
+    arrivalTime=${processSeparated[1]}
+    if [ ! $arrivalTime -gt $currentTime ] ; then
+      ARRIVED_PROCESSES+=("$process")
+      unset fileDat[$index]
     fi
   done
-  if ! $has_element_not_queued ; then
-    for process in "$fileDat[@]}"; do
-      processSeparated=($process)
-      unset ${processSeparated[5]}
-    done
-  fi
-  unset hasElementUpdated
-  for process in "${fileDat[@]}"
-    do
+  for index in "${!ARRIVED_PROCESSES[@]}"; do
+    process=${ARRIVED_PROCESSES[$index]}
+    processSeparated=($process)
+    arrivalTime=${processSeparated[1]}
+    processName=${processSeparated[0]}
+    if [ ! ${processSeparated[2]} -gt $quantum ] ; then
+      let currentTime=currentTime+processSeparated[2]
+      unset ARRIVED_PROCESSES[$index]
+      echo -n  [${processSeparated[0]}] $currentTime' '
+    else
+      let processSeparated[2]=processSeparated[2]-quantum
+      let currentTime=currentTime+quantum
+      # processSeparated[4]=$currentTime
+      process="${processSeparated[*]}"
+      ARRIVED_PROCESSES[$index]=$process
+      ARRIVED_PROCESSES+=("${ARRIVED_PROCESSES[$index]}")
+      unset ARRIVED_PROCESSES[$index]
+      echo -n  [${processSeparated[0]}] $currentTime' '
+    fi
+    sleep 0.250
+    break
+  done
+  if [ ! ${#ARRIVED_PROCESSES[@]} -gt 1 ] ; then
+    for index in "${!fileDat[@]}" ; do
+      process=${fileDat[$index]}
       processSeparated=($process)
       arrivalTime=${processSeparated[1]}
-      if  [ ! $arrivalTime -gt $currentTime ] ; then
-        processSeparated[5]=true
-        hasElementUpdated=false
-        if ! [ -z $idleTime ]; then
-          echo -n  "[*IDLE*]" $currentTime' '
-          unset idleTime
-        fi
-        let currentTime=currentTime+$(($quantum>${processSeparated[2]} ?${processSeparated[2]}:$quantum))
-
-        echo -n  [${processSeparated[0]}] $currentTime' '
-        if [ -z "${processSeparated[4]}" ]; then
-          let processSeparated[4]=${processSeparated[2]}-$quantum 
-        else
-          let processSeparated[4]=${processSeparated[4]}-$quantum 
-        fi
-        process="${processSeparated[*]}"
-        if [ ${processSeparated[4]} -lt 1 ] ; then
-          let turnAroundTime="currentTime-${processSeparated[1]}"
-          let totalWaitingTime="$totalWaitingTime + $turnAroundTime - ${processSeparated[2]}"
-          let totalTurnAroundTime="$totalTurnAroundTime + $turnAroundTime"
-          unset fileDat[$count]
-        else
-          fileDat[count]=$process
-        fi
-      fi
-      let count=count+1
-  done
-  if [ -z $hasElementUpdated ] ; then
-    idleTime=true
-    let currentTime=currentTime+1
-    unset hasElementUpdated
+      let currentTime=arrivalTime
+      echo -n [*IDLE*] $currentTime' '
+      break
+    done
   fi
-  fileDat=("${fileDat[@]}")
 done
-  let averageTurnaroundTime=totalTurnAroundTime/processCount
-  let averageWaitingTime=totalWaitingTime/processCount
-  echo
-  echo
-  echo
-  echo "Total   Turnaround Time :" $totalTurnAroundTime
-  echo "Average Turnaround Time :" $averageTurnaroundTime
-  echo "Total   Waiting Time    :" $totalWaitingTime
-  echo "Average Waiting Time    :" $averageWaitingTime
-  echo
-  echo "~~~ END: Round Robin (RRBN) Scheduling ~~~"
-  echo
 
 
 
