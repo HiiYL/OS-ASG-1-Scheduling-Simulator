76dcde0f638af43530151f82481ae2c49a3b0aed
YLHii <yonglian146@gmail.com>
2015-08-25 18:35:49 +0800

    backup

--- roundrobin.sh
+++ roundrobin.sh
@@ -1,7 +1,34 @@
 . readprocessinput.sh
 
 printFile
-
+function addProcessToArray()
+{
+  for count in "${!fileDat[@]}"; do
+    inner_process=${fileDat[$count]}
+    inner_processSeparated=($inner_process)
+    inner_arrivalTime=${inner_processSeparated[1]}
+    if [ ! $inner_arrivalTime -gt $currentTime ] ; then
+      ARRIVED_PROCESSES+=("$inner_process")
+      unset fileDat[$count]
+    fi
+  done
+}
+function checkIdle()
+{
+  if [ ! ${#ARRIVED_PROCESSES[@]} -gt 1 ]; then
+    for index in "${!fileDat[@]}" ; do
+      process=${fileDat[$index]}
+      processSeparated=($process)
+      arrivalTime=${processSeparated[1]}
+      if [ $arrivalTime -gt $currentTime ] && [ -z $objectRemoved ] ; then
+        let currentTime=arrivalTime
+        echo -n [*IDLE*] $currentTime' '
+      fi
+      unset objectRemoved
+      break
+    done
+  fi
+}
 ARRIVED_PROCESSES=()
 IFS=$'\n' 
 fileDat=($(for each in ${fileDat[@]}; do
@@ -10,15 +37,7 @@ done | sort -n -k2,2 ))
 unset IFS
 currentTime=0
 while [ ${#fileDat[@]} -gt 0 ] || [ ${#ARRIVED_PROCESSES[@]} -gt 0 ]; do
-  for index in "${!fileDat[@]}"; do
-    process=${fileDat[$index]}
-    processSeparated=($process)
-    arrivalTime=${processSeparated[1]}
-    if [ ! $arrivalTime -gt $currentTime ] ; then
-      ARRIVED_PROCESSES+=("$process")
-      unset fileDat[$index]
-    fi
-  done
+  addProcessToArray
   for index in "${!ARRIVED_PROCESSES[@]}"; do
     process=${ARRIVED_PROCESSES[$index]}
     processSeparated=($process)
@@ -28,30 +47,22 @@ while [ ${#fileDat[@]} -gt 0 ] || [ ${#ARRIVED_PROCESSES[@]} -gt 0 ]; do
       let currentTime=currentTime+processSeparated[2]
       unset ARRIVED_PROCESSES[$index]
       echo -n  [${processSeparated[0]}] $currentTime' '
+      objectRemoved=true
     else
       let processSeparated[2]=processSeparated[2]-quantum
       let currentTime=currentTime+quantum
-      # processSeparated[4]=$currentTime
       process="${processSeparated[*]}"
       ARRIVED_PROCESSES[$index]=$process
+      addProcessToArray
+      echo -n  [${processSeparated[0]}] $currentTime' '
       ARRIVED_PROCESSES+=("${ARRIVED_PROCESSES[$index]}")
       unset ARRIVED_PROCESSES[$index]
-      echo -n  [${processSeparated[0]}] $currentTime' '
     fi
-    sleep 0.250
     break
   done
-  if [ ! ${#ARRIVED_PROCESSES[@]} -gt 1 ] ; then
-    for index in "${!fileDat[@]}" ; do
-      process=${fileDat[$index]}
-      processSeparated=($process)
-      arrivalTime=${processSeparated[1]}
-      let currentTime=arrivalTime
-      echo -n [*IDLE*] $currentTime' '
-      break
-    done
-  fi
+  checkIdle
 done
+echo
 
 
 
